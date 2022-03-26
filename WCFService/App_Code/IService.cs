03ae7cfeadb8ecabd2b1bb42640fe708d3cf2497
ninama889 <ninamahemngi@gmail.com>
2022-03-26 18:56:00 +0530

    wcfsevice

---
+++ WCFService/App_Code/IService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService" in both code and config file together.
+[ServiceContract]
+public interface IService
+{
+
+	[OperationContract]
+	string Insert(InsertUser user);
+	// TODO: Add your service operations here
+}
+
+// Use a data contract as illustrated in the sample below to add composite types to service operations.
+[DataContract]
+public class InsertUser
+{
+
+	string email = string.Empty;
+	string pass = string.Empty;
+
+
+	[DataMember]
+	public string Email
+	{
+		get { return email; }
+		set { email = value; }
+	}
+	[DataMember]
+	public string Pass
+	{
+		get { return pass; }
+		set { pass = value; }
+	}
+
+}
---
+++ WCFService/App_Code/Service.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+public class Service : IService
+{
+    public string Insert(InsertUser user)
+    {
+        string msg;
+        SqlConnection con = new SqlConnection("Data Source=MSSQLLocalDB;Initial Catalog=socP;Persist Security Info=True;Pooling=False");
+        con.Open();
+        SqlCommand cmd = new SqlCommand("Insert into Table1(Email,Pass) values(@Email,@Pass)", con);
+        cmd.Parameters.AddWithValue("@Email", user.Email);
+        cmd.Parameters.AddWithValue("@Pass", user.Pass);
+
+        int g = cmd.ExecuteNonQuery();
+        if (g == 1)
+        {
+            msg = "Successfully login";
+        }
+        else
+        {
+            msg = "Failed to login ";
+        }
+        return msg;
+    }
+}
